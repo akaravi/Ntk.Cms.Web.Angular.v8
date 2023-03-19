a5b39f8c39cdb5bbba6a82e9baed493044339e18
karavi <karavi@ntk.ir>
2023-03-19 06:40:53 +0330

    indo

--- src/app/_metronic/partials/layout/theme-mode-switcher/theme-mode.module.ts
+++ src/app/_metronic/partials/layout/theme-mode-switcher/theme-mode.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { ThemeModeSwitcherComponent } from './theme-mode-switcher.component';
+import { TranslationModule } from 'src/app/modules/i18n';
 
 @NgModule({
   declarations: [ThemeModeSwitcherComponent],
-  imports: [CommonModule, InlineSVGModule],
+  imports: [CommonModule, InlineSVGModule,TranslationModule,],
   exports: [ThemeModeSwitcherComponent],
 })
 export class ThemeModeModule {}
