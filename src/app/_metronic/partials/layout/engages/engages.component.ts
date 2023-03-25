76afae45ef29541d1c890f40a12f2c804d019b35
Alireza Karavi <karavi@ntk.ir>
2023-03-25 15:09:58 +0430

    indo

--- src/app/_metronic/partials/layout/engages/engages.component.ts
+++ src/app/_metronic/partials/layout/engages/engages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-engages',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class EngagesComponent implements OnInit {
 
   constructor() { }
-
+  loadDemoTheme = environment.loadDemoTheme;
   ngOnInit(): void {
   }
 
