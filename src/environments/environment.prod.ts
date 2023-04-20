e9670cb6a767bfbb7dad31439aa9390eb8010dc8
Armin Narimani <a.narimani1998@gmail.com>
2023-04-20 12:50:06 +0330

    first narimani commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+//test karavi indonarimani
 import { EnumDeviceType, EnumOperatingSystemType } from "ntk-cms-api";
 
 export const environment = {
