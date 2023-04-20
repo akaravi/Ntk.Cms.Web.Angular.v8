c3580e8e91d5ad6ef7ac607a1a230b970db8cfef
mohamad mahvash <mohamadmahvash1@gmail.com>
2023-04-20 16:34:51 +0430

    fix bug in customer order

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { EnumDeviceType, EnumOperatingSystemType } from "ntk-cms-api";
 export const environment = {
   production: false,
   checkAccess: false,
-  appVersion: '15.1.0131.1',
+  appVersion: '15.1.1224.2',
   authKey: 'authf649fc9a5f55',
   loadDemoTheme: false,
   ProgressConsoleLog: false,
