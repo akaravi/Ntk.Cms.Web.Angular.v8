6774af02748e21eb44e5f137e45bcb4dc898aebe
siavashasachi <asachiciavash@gmail.com>
2023-05-29 12:29:54 +0330

    title changing web-desinger component

--- src/app/cms-modules/web-designer/web-designer.routing.ts
+++ src/app/cms-modules/web-designer/web-designer.routing.ts
@@ -14,98 +14,119 @@ const routes: Routes = [
   {
     path: '',
     component: WebDesignerComponent,
+    data: { title: 'ROUTE.WEBDESIGNER' },
+
     children: [
       /* Config */
       {
         path: 'config',
         loadChildren: () =>
-          import('./config/web-designer-config.module').then((m) => m.WebDesignerConfigModule),
+          import('./config/web-designer-config.module').then(
+            (m) => m.WebDesignerConfigModule
+          ),
+        data: { title: 'ROUTE.WEBDESIGNER' },
       },
       /* Config */
       /** */
       {
         path: 'intro',
-        component: WebDesignerMainIntroListComponent
+        component: WebDesignerMainIntroListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.INTRO' },
       },
       {
         path: 'intro/LinkPageId/:LinkPageId',
-        component: WebDesignerMainIntroListComponent
+        component: WebDesignerMainIntroListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.INTRO' },
       },
       {
         path: 'intro/add',
-        component: WebDesignerMainIntroAddComponent
+        component: WebDesignerMainIntroAddComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.INTRO' },
       },
       {
         path: 'intro/add/:LinkPageId',
-        component: WebDesignerMainIntroAddComponent
+        component: WebDesignerMainIntroAddComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.INTRO' },
       },
       {
         path: 'intro/edit/:Id',
-        component: WebDesignerMainIntroEditComponent
+        component: WebDesignerMainIntroEditComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.INTRO' },
       },
       /** */
       /** */
       {
         path: 'menu',
-        component: WebDesignerMainMenuListComponent
+        component: WebDesignerMainMenuListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.MENU' },
       },
       /** */
       {
         path: 'logmemberinfo',
-        component: WebDesignerLogMemberInfoListComponent
+        component: WebDesignerLogMemberInfoListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.LOGMEMBERINFO' },
       },
       /** */
       {
         path: 'pagetemplate',
-        component: WebDesignerMainPageTemplateListComponent
+        component: WebDesignerMainPageTemplateListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGETEMPLATE' },
       },
       /** */
       /** */
       {
         path: 'pagedependency',
-        component: WebDesignerMainPageDependencyListComponent
+        component: WebDesignerMainPageDependencyListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGEDEPENDENCY' },
       },
       /** */
       {
         path: 'page',
-        component: WebDesignerMainPageListComponent
+        component: WebDesignerMainPageListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       {
         path: 'page/LinkPageTemplateGuId/:LinkPageTemplateGuId',
-        component: WebDesignerMainPageListComponent
+        component: WebDesignerMainPageListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       {
         path: 'page/LinkPageParentGuId/:LinkPageParentGuId',
-        component: WebDesignerMainPageListComponent
+        component: WebDesignerMainPageListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       {
         path: 'page/LinkPageDependencyGuId/:LinkPageDependencyGuId',
-        component: WebDesignerMainPageListComponent
+        component: WebDesignerMainPageListComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       /** */
       {
         path: 'page/list-grid',
-        component: WebDesignerMainPageListGridComponent
+        component: WebDesignerMainPageListGridComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       {
         path: 'page/list-grid/LinkPageTemplateGuId/:LinkPageTemplateGuId',
-        component: WebDesignerMainPageListGridComponent
+        component: WebDesignerMainPageListGridComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       {
         path: 'page/list-grid/LinkPageParentGuId/:LinkPageParentGuId',
-        component: WebDesignerMainPageListGridComponent
+        component: WebDesignerMainPageListGridComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       {
         path: 'page/list-grid/LinkPageDependencyGuId/:LinkPageDependencyGuId',
-        component: WebDesignerMainPageListGridComponent
+        component: WebDesignerMainPageListGridComponent,
+        data: { title: 'ROUTE.WEBDESIGNER.PAGE' },
       },
       /** */
-    ]
+    ],
   },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class WebDesignerRoutes {
-}
+export class WebDesignerRoutes {}
