15e8111fb897bec24334880c92cc130bed535567
mohamad mahvash <mohamadmahvash1@gmail.com>
2023-07-13 14:48:40 +0330

    dark theme and property overview

--- src/app/cms-modules/estate/overview/events/events.component.ts
+++ src/app/cms-modules/estate/overview/events/events.component.ts
@@ -8,6 +8,15 @@ import { TranslateService } from '@ngx-translate/core';
 import {
   DataFieldInfoModel, EnumInfoModel,
   ErrorExceptionResult,
+  EstateCustomerOrderFilterModel,
+  EstateCustomerOrderModel,
+  EstateCustomerOrderService,
+  EstatePropertyFilterModel,
+  EstatePropertyHistoryFilterModel,
+  EstatePropertyHistoryModel,
+  EstatePropertyHistoryService,
+  EstatePropertyModel,
+  EstatePropertyService,
   EstatePropertyTypeUsageModel
 } from 'ntk-cms-api';
 import { Subscription } from 'rxjs';
@@ -17,10 +26,14 @@ import { ProgressSpinnerModel } from 'src/app/core/models/progressSpinnerModel';
 import { CmsToastrService } from 'src/app/core/services/cmsToastr.service';
 @Component({
   selector: 'app-estate-overview-events',
-  templateUrl: './events.component.html'
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss'],
 })
 export class EstateOverviewEventsComponent implements OnInit, OnDestroy {
   constructor(
+    public estatePropertyService: EstatePropertyService,
+    public estatePropertyHistoryService: EstatePropertyHistoryService,
+    public estateCustomerOrderService: EstateCustomerOrderService,
     public publicHelper: PublicHelper,
     private cdr: ChangeDetectorRef,
     private cmsToastrService: CmsToastrService,
@@ -33,6 +46,9 @@ export class EstateOverviewEventsComponent implements OnInit, OnDestroy {
   }
   loading = new ProgressSpinnerModel();
   dataModelResult: ErrorExceptionResult<EstatePropertyTypeUsageModel> = new ErrorExceptionResult<EstatePropertyTypeUsageModel>();
+  dataModelPropertyResult: ErrorExceptionResult<EstatePropertyModel> = new ErrorExceptionResult<EstatePropertyModel>();
+  dataModelCustomerOrderResult: ErrorExceptionResult<EstateCustomerOrderModel> = new ErrorExceptionResult<EstateCustomerOrderModel>();
+  dataModelHistoryResult: ErrorExceptionResult<EstatePropertyHistoryModel> = new ErrorExceptionResult<EstatePropertyHistoryModel>();
   fieldsInfo: Map<string, DataFieldInfoModel> = new Map<string, DataFieldInfoModel>();
   dataModelEnumRecordStatusResult: ErrorExceptionResult<EnumInfoModel> = new ErrorExceptionResult<EnumInfoModel>();
   cmsApiStoreSubscribe: Subscription;
@@ -49,12 +65,103 @@ export class EstateOverviewEventsComponent implements OnInit, OnDestroy {
 
     });
   }
+  DataGetAllProperty(): void {
+    const pName = this.constructor.name + 'main';
+
+    let filterModelOnDay = new EstatePropertyFilterModel();
+    // filterModelOnDay = filterModel;
+    if (!this.checkingOnDayRange.controls.start?.value)
+      this.checkingOnDayRange.controls.start.setValue(new Date());
+    if (!this.checkingOnDayRange.controls.end?.value)
+      this.checkingOnDayRange.controls.end.setValue(new Date());
+    // filterModelOnDay.onDateTimeFrom = this.checkingOnDayRange.controls.start.value;
+    // filterModelOnDay.onDateTimeTo = this.checkingOnDayRange.controls.end.value;
+
+    /** Search On Select Day */
+    this.estatePropertyService.ServiceGetAll(filterModelOnDay).subscribe({
+      next: (ret) => {
+        this.fieldsInfo = this.publicHelper.fieldInfoConvertor(ret.access);
+        if (ret.isSuccess) {
+          this.dataModelPropertyResult = ret;
+          console.log(this.dataModelPropertyResult);
+
+          this.loading.Stop(pName);
+        }
+        error: (er) => {
+          this.cmsToastrService.typeError(er);
+          this.loading.Stop(pName);
+        }
+      }
+    }
+    );
+  }
+  DataGetAllCustomerOrder(): void {
+    const pName = this.constructor.name + 'main';
+
+    let filterModelOnDay = new EstateCustomerOrderFilterModel();
+    // filterModelOnDay = filterModel;
+    if (!this.checkingOnDayRange.controls.start?.value)
+      this.checkingOnDayRange.controls.start.setValue(new Date());
+    if (!this.checkingOnDayRange.controls.end?.value)
+      this.checkingOnDayRange.controls.end.setValue(new Date());
+    // filterModelOnDay.onDateTimeFrom = this.checkingOnDayRange.controls.start.value;
+    // filterModelOnDay.onDateTimeTo = this.checkingOnDayRange.controls.end.value;
+
+    /** Search On Select Day */
+    this.estateCustomerOrderService.ServiceGetAll(filterModelOnDay).subscribe({
+      next: (ret) => {
+        this.fieldsInfo = this.publicHelper.fieldInfoConvertor(ret.access);
+        if (ret.isSuccess) {
+          this.dataModelCustomerOrderResult = ret;
+          console.log(this.dataModelCustomerOrderResult);
+
+          this.loading.Stop(pName);
+        }
+        error: (er) => {
+          this.cmsToastrService.typeError(er);
+          this.loading.Stop(pName);
+        }
+      }
+    }
+    );
+  }
+  DataGetAllPropertyHistory(): void {
+    const pName = this.constructor.name + 'main';
+
+    let filterModelOnDay = new EstatePropertyHistoryFilterModel();
+    // filterModelOnDay = filterModel;
+    if (!this.checkingOnDayRange.controls.start?.value)
+      this.checkingOnDayRange.controls.start.setValue(new Date());
+    if (!this.checkingOnDayRange.controls.end?.value)
+      this.checkingOnDayRange.controls.end.setValue(new Date());
+    filterModelOnDay.onDateTimeFrom = this.checkingOnDayRange.controls.start.value;
+    filterModelOnDay.onDateTimeTo = this.checkingOnDayRange.controls.end.value;
+
+    /** Search On Select Day */
+    this.estatePropertyHistoryService.ServiceGetAll(filterModelOnDay).subscribe({
+      next: (ret) => {
+        this.fieldsInfo = this.publicHelper.fieldInfoConvertor(ret.access);
+        if (ret.isSuccess) {
+          this.dataModelHistoryResult = ret;
+          console.log(this.dataModelHistoryResult);
+
+          this.loading.Stop(pName);
+        }
+        error: (er) => {
+          this.cmsToastrService.typeError(er);
+          this.loading.Stop(pName);
+        }
+      }
+    }
+    );
+  }
   ngOnDestroy(): void {
     this.cmsApiStoreSubscribe.unsubscribe();
   }
   onActionbuttonOnDateSearch() {
 
-    //this.DataGetAll();
+    this.DataGetAllProperty();
+    this.DataGetAllPropertyHistory();
   }
   onActionNext() {
     if (!this.checkingOnDayRange.controls.start?.value)
